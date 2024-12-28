a57fd5acfa46a0e335fd9371c153398125836af7
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-12-28 19:45:47 +0100

    Update layout background color from cyan to gray for improved contrast

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-      
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-cyan-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900`}
       >
         <SideBar/>
         {children}
