5d9acd2c9daa00129f07adfc3b03ca47107b78c5
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-01-15 17:16:44 +0100

    Adjust grid layout in MovieGrid component for improved responsiveness

--- components/movies/movie-grid.tsx
+++ components/movies/movie-grid.tsx
@@ -84,7 +84,7 @@ const MovieGrid: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {movieData.map((movie) => (
           <MovieCard
             key={movie.key}
